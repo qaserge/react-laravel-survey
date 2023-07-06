79d5fe7884792ae040d3045787775c0cb7ae005e
Sergei <qaserge@gmail.com>
2023-07-06 13:22:50 -0400

    Create new Survey form with dummy method created

--- vite-project/src/views/Surveys.jsx
+++ vite-project/src/views/Surveys.jsx
@@ -1,5 +1,7 @@
+import { PlusCircleIcon } from "@heroicons/react/24/outline";
 import PageComponent from "../components/PageComponent";
 import SurveyListItem from "../components/SurveyListItem";
+import TButton from "../components/core/TButton";
 import { useStateContext } from "./contexts/ContextProvider";
 
 export default function Surveys() {
@@ -11,7 +13,14 @@ export default function Surveys() {
   }
 
   return (
-    <PageComponent title="Surveys" >
+    <PageComponent
+      title="Surveys"
+      buttons={(
+        <TButton color="green" to="/surveys/create">
+          <PlusCircleIcon className="h-6 w-6 mr-2" />
+          Create new
+        </TButton>
+      )}>
       <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3">
         {surveys.map(survey => (
           // key не нужно объявлять в пропсах SurveyListItem, потому что это уже предопределено в react
